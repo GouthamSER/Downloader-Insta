f946c5bda4c6ceb4ad3a6ab17ad65c0e09189377
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2024-06-14 06:39:45 +0530

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,22 @@
+class script(object):
+  
+  START_TXT = """{} {}
+HI  I am Goutham Ser Bot
+This is MAde From Pyrogram and i am studying this language
+All CopyRights TO Goutham Josh
+ 
+@im_goutham_josh
+"""
+
+  HELP_TXT="""
+This is a studiying pyrogram bot
+<u>developed by Profile Photo Ittekunavan</u>
+"""
+
+  ABOUT_TXT="""
+✯ Cʀᴇᴀᴛᴏʀ: Gᴏᴜᴛʜᴀᴍ Sᴇʀ
+✯ Lɪʙʀᴀʀʏ: Pʏʀᴏɢʀᴀᴍ
+✯ Lᴀɴɢᴜᴀɢᴇ: Pʏᴛʜᴏɴ 3
+✯ Bᴏᴛ Sᴇʀᴠᴇʀ: RAILWAY
+"""
+

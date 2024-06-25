3774f5db11275eee687feb8440890c2ee134e6d0
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2024-06-25 08:53:15 +0530

    Create bot2.py

---
+++ bot2.py
@@ -0,0 +1,22 @@
+def download_video(url, format="mp4"):
+    ydl_opts = {
+        'format': format,
+        'outtmpl': '%(title)s.%(ext)s'
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=True)
+        return ydl.prepare_filename(info)
+
+def download_audio(url, format="bestaudio"):
+    ydl_opts = {
+        'format': format,
+        'outtmpl': '%(title)s.%(ext)s',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=True)
+        return ydl.prepare_filename(info)
